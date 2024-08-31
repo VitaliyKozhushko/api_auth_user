6ca72823505cc19dbad2e5ad952c141e50cbbd5f
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-08-31 11:41:55 +0300

    create api registration user

---
+++ users/serializers.py
@@ -0,0 +1,45 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueValidator
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+  email = serializers.EmailField(
+    required=False,
+    validators=[UniqueValidator(queryset=User.objects.all())],
+    allow_blank=True
+  )
+
+  phone = serializers.CharField(
+    required=False,
+    allow_blank=True
+  )
+
+  password = serializers.CharField(
+    write_only=True,
+    validators=[validate_password],
+    required=True
+  )
+
+  repeat_password = serializers.CharField(write_only=True, required=True)
+
+  class Meta:
+    model = User
+    fields =('username', 'phone', 'email', 'password', 'repeat_password')
+
+  def validate(self, attrs):
+    if attrs['password'] != attrs['repeat_password']:
+      raise serializers.ValidationError({'password': 'Пароли не совпадают'})
+    return attrs
+
+  def create(self, validated_data):
+    user = User(
+      username=validated_data['username'],
+      phone=validated_data.get('phone'),
+      email=validated_data.get('email')
+    )
+    user.set_password(validated_data['password'])
+    user.save()
+    return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import User
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class RegistrationView(generics.CreateAPIView):
+  queryset = User.objects.all()
+  permission_classes = [AllowAny]
+  serializer_class = RegisterSerializer
+
+  def create(self, request, *args, **kwargs):
+    serializer =self.get_serializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    headers = self.get_success_headers(serializer.data)
+    response_data = {
+      'status':'success',
+      'message': 'Пользователь успешно зарегистрирован',
+      'data': {
+        'user_id': user.id,
+        'login': user.username,
+        'phone': user.phone,
+        'email': user.email
+      }
+    }
+    return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
