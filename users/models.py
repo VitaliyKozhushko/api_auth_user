cd0cad91f75c271b656fce2f5bbad15ca4086a00
VitaliyKozhushko <pelevinastudio@yandex.ru>
2024-08-31 10:27:46 +0300

    create User model add ForeignKey

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+  phone = models.CharField(max_length=15, unique=True, blank=True, null=True)
+  email = models.EmailField(unique=True, blank=True, null=True)
+  is_mentor = models.BooleanField(default=False)
+
+  mentor = models.ForeignKey(
+    'self',
+    on_delete=models.SET_NULL,
+    related_name='mentees',
+    null=True,
+    blank=True,
+  )
